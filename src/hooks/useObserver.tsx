bff0d390eee7394fd142bc0c5b3f5dd734183aee
Ali Sasani Babak <ababak@nuclera.com>
2024-08-24 14:38:13 +0100

    add useObserver hook to detect element reaching to view

---
+++ src/hooks/useObserver.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+
+// Custom Hook: Handle Intersection Observer for Infinite Scrolling
+const useObserver = (isInfiniteScrolling: boolean, loadMore: () => void) => {
+  const observerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (isInfiniteScrolling && observerRef.current) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          const target = entries[entries.length - 1];
+          if (target.isIntersecting) {
+            loadMore();
+          }
+        },
+        { threshold: 0.9 }
+      );
+
+      observer.observe(observerRef.current);
+
+      return () => observer.disconnect();
+    }
+  }, [loadMore, isInfiniteScrolling]);
+
+  return observerRef;
+};
+
+export default useObserver;
