18722b43f7c78fcbe3070faf03f1915299012d0b
Ali Sasani Babak <ababak@nuclera.com>
2024-08-24 13:45:47 +0100

    add modal

---
+++ src/components/common/modal/index.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import CustomButton from "../button";
+
+interface ModalProps {
+  visible: boolean;
+  handleClose: () => void;
+  onOK: () => void;
+  handleCancel?: () => void;
+  title?: string;
+  content: React.ReactNode;
+  oKText?: string;
+  cancelText?: string;
+  width?: string;
+  height?: string;
+  maxWidth?: string;
+  maxHeight?: string;
+}
+
+export default function Modal({
+  visible,
+  handleClose,
+  onOK,
+  handleCancel,
+  title,
+  content,
+  oKText = "OK",
+  cancelText = "Cancel",
+  width = "800px",
+  height = "300px",
+  maxWidth = "80%",
+  maxHeight = "70%",
+}: ModalProps) {
+  return (
+    <Dialog
+      PaperProps={{
+        sx: {
+          width,
+          height,
+          maxWidth,
+          maxHeight,
+          padding: "20px",
+        },
+      }}
+      open={visible}
+      onClose={handleClose}
+      aria-labelledby='alert-dialog-title'
+      aria-describedby='alert-dialog-description'>
+      {title && <DialogTitle id='alert-dialog-title'>{title}</DialogTitle>}
+      <DialogContent>
+        <DialogContentText id='alert-dialog-description'>{content}</DialogContentText>
+      </DialogContent>
+      <DialogActions sx={{ display: "flex", gap: "20px" }}>
+        {cancelText && (
+          <CustomButton
+            onClick={handleCancel}
+            aria-label='Cancel'>
+            {cancelText}
+          </CustomButton>
+        )}
+        <CustomButton
+          variant='contained'
+          onClick={onOK}
+          autoFocus
+          aria-label='Confirm'>
+          {oKText}
+        </CustomButton>
+      </DialogActions>
+    </Dialog>
+  );
+}
